8df30e75ffcb4f4999721b660dcfb715c7958796
kruschwitzm <magnus.kruschwitz@i12.de>
2018-09-09 23:18:08 +0200

    Initial Commit

---
+++ OBJC04A-Einsendearbeit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OBJC04A-Einsendearbeit
+//
+//  Created by Magnus Kruschwitz on 09.09.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
